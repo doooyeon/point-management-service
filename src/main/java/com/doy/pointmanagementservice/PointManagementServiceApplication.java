c94df366a4f478d5a3abe4f51b32f344275c171a
doooyeon <ehdus5958@naver.com>
2018-10-16 01:30:04 +0900

    Initial project

---
+++ src/main/java/com/doy/pointmanagementservice/PointManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.doy.pointmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointManagementServiceApplication.class, args);
+	}
+}
